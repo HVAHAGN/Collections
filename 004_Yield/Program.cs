7e0d18619f88cc571be9873474f597cac89c2153
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-05-01 00:43:17 +0400

    IEnumerable Power()

---
+++ 004_Yield/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_Yield
+{
+    class Program
+    {
+        static void Main()
+        {
+            foreach (string element in UserCollection.Power())
+                Console.Write(element);
+
+            // Delay. 
+            Console.ReadKey();
+        }
+    }
+}
