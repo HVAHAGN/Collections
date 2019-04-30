853280e25b3b2708d5e3cb3d2251e7190f89d1a4
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-30 20:04:56 +0400

    private bool IEnumerator.MoveNext() // Так в Рефлекторе

---
+++ 002_Yield/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace _002_Yield
+{
+    class Program
+    {
+        static void Main()
+        {
+            foreach (string element in UserCollection.Power())
+                Console.WriteLine(element);
+
+            Console.WriteLine(new string('-', 12));
+
+            //-----------------------------------------------------------------------------------------------
+            // Так работает foreach.
+
+            IEnumerable enumerable = UserCollection.Power();
+
+            IEnumerator enumerator = enumerable.GetEnumerator();
+            enumerator.Reset();
+
+            while (enumerator.MoveNext()) // Перемещаем курсор на 1 шаг вперед.
+            {
+                String element = enumerator.Current as String;
+
+                Console.WriteLine(element);
+            }
+
+            // Delay. 
+            Console.ReadKey();
+        }
+    }
+}
