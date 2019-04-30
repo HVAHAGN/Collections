a2529618b3945d8722712aef1d3c99beb3396591
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-30 20:29:23 +0400

    yield break

---
+++ 003-Yeild/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_Yeild
+{
+    class Program
+    {
+        static void Main()
+        {
+            foreach (string element in UserCollection.Power())
+                Console.Write(element);
+
+            // Delay. 
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 003-Yeild/UserCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace _003_Yeild
+{
+    class UserCollection
+    {
+        public static IEnumerable Power()
+        {
+            yield break;
+        }
+    }
+}
