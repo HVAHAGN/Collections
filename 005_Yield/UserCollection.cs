21e9eac1e04ba7c1ad693658e8e5beba6bd4ef15
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-05-01 01:12:19 +0400

    Yield example 5

---
+++ 005_Yield/UserCollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace _005_Yield
+{
+    class UserCollection
+    {
+
+        public static IEnumerable Power()
+        {
+            while (true)
+                yield return "Hello world!";
+        }
+    }
+}
