ead5307603ffe41482086cc84d8a4f09906e2e6f
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-05-01 01:18:15 +0400

    Yield Example N6

---
+++ 006_Yield/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006_Yield
+{
+    class Program
+    {
+        static void Main()
+        {
+            foreach (string element in UserCollection.Power())
+                Console.WriteLine(element);
+                       
+            // Delay. 
+            Console.ReadKey();
+        }
+}
+
