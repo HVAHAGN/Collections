782b077a5d649eb7476745b84abd9f728a35e4a2
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-30 19:41:30 +0400

    ератор по сути представляет блок кода, который использует оператор yield для перебора набора значений. Данный блок кода может представлять тело метода, оператора или блок get в свойства

--- 001_Yield/Program.cs
+++ 001_Yield/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace _001_Yield
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            foreach (string element in UserCollection.Power())
+                Console.WriteLine(element);
+
+            Console.WriteLine(new string('-', 12));
+
+            //-----------------------------------------------------------------------------------------------
+            // Так работает foreach.
+
+            IEnumerable enumerable = UserCollection.Power();
+
+            IEnumerator enumerator = enumerable.GetEnumerator();
+
+            while (enumerator.MoveNext()) // Перемещаем курсор на 1 шаг вперед.
+            {
+                String element = enumerator.Current as String;
+
+                Console.WriteLine(element);
+            }
+
+            // Delay. 
+            Console.ReadKey();
         }
     }
 }
